e0bf54c867fbf975fb9b2b1dd9f8ca276242c662
Kevin D Goveas <kevdanngovead@gmail.com>
2025-07-05 17:54:18 +0200

    i like this stage the best

--- start-cluster.sh
+++ start-cluster.sh
@@ -6,16 +6,37 @@ docker compose -p redis-cluster down
 echo -e "Composing Docker Network: \n"
 docker compose -p redis-cluster up -d
 
-sleep 10
+sleep 5
 
 echo -e "\nCreating Redis Cluster with 9 nodes (3 masters, 6 slaves): \n"
 docker exec master-A redis-cli --cluster create \
   master-A:6379 master-B:6379 master-C:6379 \
-  slave-A1:6379 slave-B1:6379 slave-C1:6379 \
-  slave-A2:6379 slave-B2:6379 slave-C2:6379 \
-  --cluster-replicas 2 --cluster-yes
+  --cluster-yes
 
-sleep 10
+sleep 1
+docker exec slave-A1 redis-cli --cluster add-node \
+  slave-A1:6379 master-A:6379 --cluster-slave
 
+sleep 1
+docker exec slave-A2 redis-cli --cluster add-node \
+  slave-A2:6379 master-A:6379 --cluster-slave
+
+sleep 1
+docker exec slave-B1 redis-cli --cluster add-node \
+  slave-B1:6379 master-B:6379 --cluster-slave
+
+sleep 1
+docker exec slave-B2 redis-cli --cluster add-node \
+  slave-B2:6379 master-B:6379 --cluster-slave
+
+sleep 1
+docker exec slave-C1 redis-cli --cluster add-node \
+  slave-C1:6379 master-C:6379 --cluster-slave
+
+sleep 1
+docker exec slave-C2 redis-cli --cluster add-node \
+  slave-C2:6379 master-C:6379 --cluster-slave
+
+sleep 2
 echo -e "\nConnecting to Redis Cluster: \n"
 docker exec -it master-A redis-cli -c -p 6379
