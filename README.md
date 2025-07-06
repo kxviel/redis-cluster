37e8cf26cca2ab3390fd01a943aaebab2e303058
Kevin D Goveas <kevdanngovead@gmail.com>
2025-07-06 20:09:20 +0200

    final change to startup

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### Replication:
 
 1. on a terminal, run `bash start-cluster.sh` to start cluster on docker
-2. on a new terminal, run `docker exec -it redis-client sh -c "node index.ts"` to run the code
+2. on a new terminal, run `docker exec redis-client sh -c "node index.ts"` to run the code
 3. once the keys are added, on a new terminal, run `bash get-dbsize`
 
 ### Availabilty:
--- start-cluster.sh
+++ start-cluster.sh
@@ -16,9 +16,9 @@ echo -e "\nWaiting for master cluster to stabilize...\n"
 sleep 5
 
 echo -e "Getting master node IDs...\n"
-MASTER_A_ID=$(docker exec master-A redis-cli cluster nodes | grep "master-A:6379.*master" | awk '{print $1}')
-MASTER_B_ID=$(docker exec master-B redis-cli cluster nodes | grep "master-B:6379.*master" | awk '{print $1}')
-MASTER_C_ID=$(docker exec master-C redis-cli cluster nodes | grep "master-C:6379.*master" | awk '{print $1}')
+MASTER_A_ID=$(docker exec master-A redis-cli CLUSTER MYID)
+MASTER_B_ID=$(docker exec master-B redis-cli CLUSTER MYID)
+MASTER_C_ID=$(docker exec master-C redis-cli CLUSTER MYID)
 
 echo "Master A ID: $MASTER_A_ID"
 echo "Master B ID: $MASTER_B_ID"
@@ -27,31 +27,31 @@ echo "Master C ID: $MASTER_C_ID"
 echo -e "\nAdding slaves to Master A:\n"
 docker exec slave-A1 redis-cli --cluster add-node \
   slave-A1:6379 master-A:6379 --cluster-slave --cluster-master-id $MASTER_A_ID
-sleep 2
-
-docker exec slave-A2 redis-cli --cluster add-node \
-  slave-A2:6379 master-A:6379 --cluster-slave --cluster-master-id $MASTER_A_ID
-sleep 2
+sleep 5
 
 echo -e "\nAdding slaves to Master B:\n"
 docker exec slave-B1 redis-cli --cluster add-node \
   slave-B1:6379 master-B:6379 --cluster-slave --cluster-master-id $MASTER_B_ID
-sleep 2
-
-docker exec slave-B2 redis-cli --cluster add-node \
-  slave-B2:6379 master-B:6379 --cluster-slave --cluster-master-id $MASTER_B_ID
-sleep 2
+sleep 5
 
 echo -e "\nAdding slaves to Master C:\n"
 docker exec slave-C1 redis-cli --cluster add-node \
   slave-C1:6379 master-C:6379 --cluster-slave --cluster-master-id $MASTER_C_ID
-sleep 2
+sleep 5
+
+docker exec slave-A2 redis-cli --cluster add-node \
+  slave-A2:6379 master-A:6379 --cluster-slave --cluster-master-id $MASTER_A_ID
+sleep 5
+
+docker exec slave-B2 redis-cli --cluster add-node \
+  slave-B2:6379 master-B:6379 --cluster-slave --cluster-master-id $MASTER_B_ID
+sleep 5
 
 docker exec slave-C2 redis-cli --cluster add-node \
   slave-C2:6379 master-C:6379 --cluster-slave --cluster-master-id $MASTER_C_ID
 
-echo -e "\nWaiting for cluster to stabilize...\n"
-sleep 3
+echo -e "\nWaiting 15 secs for cluster to stabilize...\n"
+sleep 15
 
 echo -e "Redis Cluster setup complete!\n"
 echo "Cluster status:"
