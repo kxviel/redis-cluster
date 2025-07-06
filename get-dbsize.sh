67452e2249be050a5e421b2dfdc5e152ad091141
Kevin D Goveas <kevdanngovead@gmail.com>
2025-07-06 11:45:31 +0200

    fix: get db bash

--- get-dbsize.sh
+++ get-dbsize.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-docker exec -it master-A redis-cli dbsize
-sleep 2
+echo -e "\nMaster-A: $(docker exec -it master-A redis-cli dbsize)"
+sleep 1
 
-docker exec -it slave-A1 redis-cli dbsize
-sleep 2
+echo -e "Slave-A1: $(docker exec -it slave-A1 redis-cli dbsize)"
+sleep 1
 
-docker exec -it slave-A2 redis-cli dbsize
-sleep 2
+echo -e "Slave-A2: $(docker exec -it slave-A2 redis-cli dbsize)"
+sleep 1
 
-docker exec -it master-B redis-cli dbsize
-sleep 2
+echo -e "\nMaster-B: $(docker exec -it master-B redis-cli dbsize)"
+sleep 1
 
-docker exec -it slave-B1 redis-cli dbsize
-sleep 2
+echo -e "Slave-B1: $(docker exec -it slave-B1 redis-cli dbsize)"
+sleep 1
 
-docker exec -it slave-B2 redis-cli dbsize
-sleep 2
+echo -e "Slave-B2: $(docker exec -it slave-B2 redis-cli dbsize)"
+sleep 1
 
-docker exec -it master-C redis-cli dbsize
-sleep 2
+echo -e "\nMaster-C: $(docker exec -it master-C redis-cli dbsize)"
+sleep 1
 
-docker exec -it slave-C1 redis-cli dbsize
-sleep 2
+echo -e "Slave-C1: $(docker exec -it slave-C1 redis-cli dbsize)"
+sleep 1
 
-docker exec -it slave-C2 redis-cli dbsize
-sleep 2
+echo -e "Slave-C2: $(docker exec -it slave-C2 redis-cli dbsize)"
+sleep 1
+
+MASTER_A=$(docker exec master-A redis-cli dbsize)
+MASTER_B=$(docker exec master-B redis-cli dbsize)
+MASTER_C=$(docker exec master-C redis-cli dbsize)
+TOTAL=$((MASTER_A + MASTER_B + MASTER_C))
+echo -e "\nTotal Keys: $TOTAL\n"
+sleep 1
